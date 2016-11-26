518f798b939f7f74c41ef8953dc53acc9387ce91
Moujimouja <egautier@centreon.com>
2016-11-26 23:58:25 +0100

    try and try

--- scripts/debug-script.sh
+++ scripts/debug-script.sh
@@ -6,6 +6,16 @@ set -x
 service mysql start
 httpd -k start
 
+cd /usr/share/centreon/www/install/steps/process
+cat ../../../../autoinstall.php installConfigurationDb.php | php
+cat ../../../../autoinstall.php installStorageDb.php | php
+cat ../../../../autoinstall.php createDbUser.php | php
+cat ../../../../autoinstall.php insertBaseConf.php | php
+cat ../../../../autoinstall.php configFileSetup.php | php
+cat ../../../../autoinstall.php partitionTables.php | php
+
+
+
 service mysql stop
 httpd -k stop
 
