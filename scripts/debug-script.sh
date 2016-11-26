80ea368b0df9c2c3732b2acdebbbd6a96f232299
Moujimouja <egautier@centreon.com>
2016-11-27 00:12:35 +0100

    tryin

--- scripts/debug-script.sh
+++ scripts/debug-script.sh
@@ -6,14 +6,7 @@ set -x
 service mysql start
 httpd -k start
 
-cd /usr/share/centreon/www/install/steps/process
-cat ../../../../autoinstall.php installConfigurationDb.php | php
-cat ../../../../autoinstall.php installStorageDb.php | php
-cat ../../../../autoinstall.php createDbUser.php | php
-cat ../../../../autoinstall.php insertBaseConf.php | php
-cat ../../../../autoinstall.php configFileSetup.php | php
-cat ../../../../autoinstall.php partitionTables.php | php
-
+mysql -e "GRANT ALL ON *.* to root@'%' IDENTIFIED BY 'centreon'"
 
 
 service mysql stop
